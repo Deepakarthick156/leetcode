0c0020d94e089eb1e796a10b553828553a1d3157
Deepakarthick156 <kdeepakr156@gmail.com>
2026-08-13 14:14:17 +0530

    Time: 1 ms (97.96%), Space: 46.6 MB (19.59%) - LeetHub

---
+++ 3142-check-if-grid-satisfies-conditions/3142-check-if-grid-satisfies-conditions.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean satisfiesConditions(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(j+1<n && grid[i][j]==grid[i][j+1])return false;
+                if(i+1<m && grid[i][j]!=grid[i+1][j])return false;
+            }
+        }
+        return true;
+    }
+}

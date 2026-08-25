2301410f92175c14ec3c65073cac319c6af3a128
Deepakarthick Raja E <kdeepakr156@gmail.com>
2026-08-25 09:54:22 +0530

    Time: 218 ms (5.11%), Space: 41.9 MB (97.86%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.java
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] constructRectangle(int area) {
-        if(area==1)return new int[]{1,1};
+        
         for(int i=area-1;i>=1;i--){
             if(area%i==0){
                 int q=area/i;
@@ -9,6 +9,6 @@ class Solution {
                 }
             }
         }
-        return new int[]{0,0};
+        return new int[]{1,1};
     }
 }
